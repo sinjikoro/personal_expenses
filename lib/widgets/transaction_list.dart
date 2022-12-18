ceaf3412c5fdce50f2364c16123adf90965fac0b
sinjikoro <shinnaga.dev@gmail.com>
2022-12-19 02:17:45 +0900

    91. Working with ListViews

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,44 +10,47 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: transactions.map((tx) {
-      return Card(
-        child: Row(children: [
-          Container(
-            margin: const EdgeInsets.symmetric(
-              vertical: 10,
-              horizontal: 15,
-            ),
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black, width: 2)),
-            padding: const EdgeInsets.all(10),
-            child: Text(
-              '\$${tx.amount}',
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Colors.purple,
-              ),
-            ),
-          ),
-          Column(
-            children: [
-              Text(
-                tx.title,
-                style: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                DateFormat.yMMMd().format(tx.date),
-                style: const TextStyle(color: Colors.grey),
-              ),
-            ],
-          )
-        ]),
-      );
-    }).toList());
+    return SizedBox(
+        height: 300,
+        child: ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: (ctx, index) {
+              return Card(
+                child: Row(children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 2)),
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      '\$${transactions[index].amount}}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        transactions[index].title,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        DateFormat.yMMMd().format(transactions[index].date),
+                        style: const TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  )
+                ]),
+              );
+            }));
   }
 }
