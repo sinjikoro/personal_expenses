03101ebe7ce3d283fe26b17c5bbb961311a49d4c
sinjikoro <shinnaga.dev@gmail.com>
2022-12-20 01:12:20 +0900

    105. Populating the Chart with Values

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -12,17 +12,20 @@ class ChartBar extends StatelessWidget {
     return Column(
       children: <Widget>[
         Text('\$${spendingAmount.toStringAsFixed(0)}'),
-        const SizedBox(height: 4,),
+        const SizedBox(
+          height: 4,
+        ),
         SizedBox(
           height: 60,
           width: 10,
           child: Stack(
             children: <Widget>[
-              Container(decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey, width: 1.0),
-                color: const Color.fromRGBO(220, 220, 220, 1),
-                borderRadius: BorderRadius.circular(10),
-              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1.0),
+                  color: const Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
               ),
               FractionallySizedBox(
                 heightFactor: spendingPctOfTotal,
@@ -32,11 +35,13 @@ class ChartBar extends StatelessWidget {
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-                ),
+              ),
             ],
           ),
-        )
+        ),
+        const SizedBox(height: 4),
+        Text(label),
       ],
-    )
+    );
   }
 }
