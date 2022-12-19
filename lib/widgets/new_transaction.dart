c4436bc765e7ec596ccc1acad3324b379b46686f
sinjikoro <shinnaga.dev@gmail.com>
2022-12-19 11:53:57 +0900

    95. Improving & Styling the Modal Bottom Sheet

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
   final Function addTx;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
 
   NewTransaction({Key? key, required this.addTx}) : super(key: key);
 
+  @override
+  State<NewTransaction> createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+
+  final amountController = TextEditingController();
+
   void submitData() {
     final enteredTitle = titleController.text;
-    final enteredAmount = double.parse(amountController.text);
+    final enteredAmount = double.tryParse(amountController.text) ?? 0;
 
     debugPrint('testPrint');
     debugPrint(enteredTitle);
@@ -18,7 +25,9 @@ class NewTransaction extends StatelessWidget {
       return;
     }
 
-    addTx(enteredTitle, enteredAmount);
+    widget.addTx(enteredTitle, enteredAmount);
+
+    Navigator.of(context).pop();
   }
 
   @override
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -16,14 +16,14 @@ class TransactionList extends StatelessWidget {
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
               return Card(
-                child: Row(children: [
+                child: Row(children: <Widget>[
                   Container(
                     margin: const EdgeInsets.symmetric(
                       vertical: 10,
                       horizontal: 15,
                     ),
                     decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black, width: 2)),
+                        border: Border.all(color: Colors.purple, width: 2)),
                     padding: const EdgeInsets.all(10),
                     child: Text(
                       '\$${transactions[index].amount.toStringAsFixed(2)}',
@@ -35,11 +35,12 @@ class TransactionList extends StatelessWidget {
                     ),
                   ),
                   Column(
-                    children: [
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
                       Text(
                         transactions[index].title,
                         style: const TextStyle(
-                          fontSize: 18,
+                          fontSize: 16,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
