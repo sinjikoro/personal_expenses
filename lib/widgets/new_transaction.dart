c9b36e2cb078b3e9a6215a7d66dff472be19e92b
sinjikoro <shinnaga.dev@gmail.com>
2022-12-26 01:26:16 +0900

    128. Respecting the Softkeyboard Insets

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -51,48 +51,54 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Container(
-        padding: const EdgeInsets.all(10),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-          TextField(
-            decoration: const InputDecoration(labelText: 'Title'),
-            controller: _titleController,
-            onSubmitted: (_) => _submitData(),
-          ),
-          TextField(
-            decoration: const InputDecoration(labelText: 'Amount'),
-            controller: _amountController,
-            onSubmitted: (_) => _submitData(),
-          ),
-          SizedBox(
-            height: 70,
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: Text(_selectedDate == null
-                      ? 'No Date Chosen!'
-                      : 'Picked date : ${DateFormat.yMd().format(_selectedDate ?? DateTime.now())}'),
-                ),
-                TextButton(
-                  onPressed: _presentDatePicker,
-                  style: ButtonStyle(
-                      foregroundColor: MaterialStateProperty.all<Color>(
-                          Theme.of(context).primaryColor)),
-                  child: const Text(
-                    'Choose Date',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ],
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(
+              top: 10,
+              left: 10,
+              right: 10,
+              bottom: MediaQuery.of(context).viewInsets.bottom + 10),
+          child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+            TextField(
+              decoration: const InputDecoration(labelText: 'Title'),
+              controller: _titleController,
+              onSubmitted: (_) => _submitData(),
             ),
-          ),
-          ElevatedButton(
-            onPressed: _submitData,
-            child: const Text('Add Transaction'),
-          )
-        ]),
+            TextField(
+              decoration: const InputDecoration(labelText: 'Amount'),
+              controller: _amountController,
+              onSubmitted: (_) => _submitData(),
+            ),
+            SizedBox(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(_selectedDate == null
+                        ? 'No Date Chosen!'
+                        : 'Picked date : ${DateFormat.yMd().format(_selectedDate ?? DateTime.now())}'),
+                  ),
+                  TextButton(
+                    onPressed: _presentDatePicker,
+                    style: ButtonStyle(
+                        foregroundColor: MaterialStateProperty.all<Color>(
+                            Theme.of(context).primaryColor)),
+                    child: const Text(
+                      'Choose Date',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: _submitData,
+              child: const Text('Add Transaction'),
+            )
+          ]),
+        ),
       ),
     );
   }
