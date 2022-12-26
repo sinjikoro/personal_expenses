5640bef3e3366315947c17ef1a6969e677874ced
sinjikoro <shinnaga.dev@gmail.com>
2022-12-27 01:08:22 +0900

    134. More Cupertino Styles

--- lib/main.dart
+++ lib/main.dart
@@ -145,7 +145,10 @@ class _MyHomePageState extends State<MyHomePage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                const Text('Show Chart'),
+                Text(
+                  'Show Chart',
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
                 Switch.adaptive(
                     value: _showChart,
                     onChanged: (val) {
